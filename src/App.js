4c47075a6969fb971f80f34ab33358a5dc63d495
Sooyong Wang <wangsy@mintech.kr>
2022-04-17 22:55:45 +0900

    Color List and useState only in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import "./components/StarRating";
-import StarRating from './components/StarRating';
+import React, { useState } from "react";
+import ColorData from "./data/color-data.json";
+import ColorList from "./components/ColorList";
 
 function App() {
-  return (
-    <div className="App">
-      <StarRating style={{ backgroundColor: "lightblue" }} onDoubleClick={ e => alert("double click")}/>
-    </div>
-  );
+  const [colors, setColors] = useState(ColorData);
+  return <ColorList 
+    colors={colors}
+    onRemoveColor={ id => {
+      const newColors = colors.filter(color => color.id !== id);
+      setColors(newColors);
+    }}
+    onRateColor={(id, rating) => {
+      const newColors = colors.map( color => 
+        color.id === id ? { ...color, rating} : color
+      );
+      setColors(newColors);
+    }}
+  />;
 }
 
 export default App;
--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -6,16 +6,15 @@ const createArray = length => [...Array(length)];
 const Star = ({ selected = false, onSelect = f => f}) => (
   <FaStar color={selected ? "red" : "gray"} onClick={onSelect} />
 );
-export default function StarRating ({style = {}, totalStars = 5, ...props}) {
-  const [selectedStars, setSelectedStars] = useState(0);
 
+export default function StarRating ({selectedStars = 0, style = {}, totalStars = 5, onRate= f => f}) {
   return (
-    <div style={{ padding: "5px", ...style }} {...props}>
+    <div style={{ padding: "5px", ...style }}>
       {createArray(totalStars).map((n, i) => (
         <Star 
           key={i} 
           selected={selectedStars > i}
-          onSelect={() => setSelectedStars(i+1)} />
+          onSelect={() => onRate(i+1)} />
       ))}
       <p>
         {selectedStars} of {totalStars} stars
